7616a47f77c104e1abe7e3c8f379f1ad8d3990d7
David Narvaez <dnarvaez27@outlook.com>
2019-06-22 14:10:09 -0500

    SQL script

--- script.sql
+++ script.sql
@@ -1,6 +1,6 @@
 -- ************************************** `USER`
 
-CREATE TABLE `USER`
+CREATE TABLE `user`
 (
  `ID`       integer NOT NULL AUTO_INCREMENT ,
  `NAME`     varchar(45) NOT NULL ,
@@ -11,39 +11,39 @@ CREATE TABLE `USER`
  `BIRTHDAY` date NULL ,
  `ABOUT_ME` varchar(255) NULL ,
 
-PRIMARY KEY (`ID`)
+  PRIMARY KEY (`ID`)
 );
 
 
 -- ************************************** `FRIENDSHIP`
 
-CREATE TABLE `FRIENDSHIP`
+CREATE TABLE `friendship`
 (
  `ID_SENDER`    integer NOT NULL ,
  `ACCEPTED`     smallint NOT NULL ,
  `ID_RECIPIENT` integer NOT NULL ,
 
-PRIMARY KEY (`ID_SENDER`, `ID_RECIPIENT`),
-KEY `fkIdx_60` (`ID_SENDER`),
-CONSTRAINT `FK_60` FOREIGN KEY `fkIdx_60` (`ID_SENDER`) REFERENCES `USER` (`ID`),
-KEY `fkIdx_63` (`ID_RECIPIENT`),
-CONSTRAINT `FK_63` FOREIGN KEY `fkIdx_63` (`ID_RECIPIENT`) REFERENCES `USER` (`ID`)
+  PRIMARY KEY (`ID_SENDER`, `ID_RECIPIENT`),
+  KEY `fkIdx_60` (`ID_SENDER`),
+  CONSTRAINT `FK_60` FOREIGN KEY `fkIdx_60` (`ID_SENDER`) REFERENCES `user` (`ID`),
+  KEY `fkIdx_63` (`ID_RECIPIENT`),
+  CONSTRAINT `FK_63` FOREIGN KEY `fkIdx_63` (`ID_RECIPIENT`) REFERENCES `user` (`ID`)
 );
 
 -- ************************************** `POST`
 
-CREATE TABLE `POST`
+CREATE TABLE `post`
 (
  `ID`           integer NOT NULL AUTO_INCREMENT ,
  `CONTENT`      varchar(255) NOT NULL ,
  `CREATOR_ID`   integer NOT NULL ,
  `RECIPIENT_ID` integer NOT NULL ,
 
-PRIMARY KEY (`ID`),
-KEY `fkIdx_43` (`CREATOR_ID`),
-CONSTRAINT `FK_43` FOREIGN KEY `fkIdx_43` (`CREATOR_ID`) REFERENCES `USER` (`ID`),
-KEY `fkIdx_46` (`RECIPIENT_ID`),
-CONSTRAINT `FK_46` FOREIGN KEY `fkIdx_46` (`RECIPIENT_ID`) REFERENCES `USER` (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `fkIdx_43` (`CREATOR_ID`),
+  CONSTRAINT `FK_43` FOREIGN KEY `fkIdx_43` (`CREATOR_ID`) REFERENCES `user` (`ID`),
+  KEY `fkIdx_46` (`RECIPIENT_ID`),
+  CONSTRAINT `FK_46` FOREIGN KEY `fkIdx_46` (`RECIPIENT_ID`) REFERENCES `user` (`ID`)
 );
 
 
@@ -53,32 +53,32 @@ CONSTRAINT `FK_46` FOREIGN KEY `fkIdx_46` (`RECIPIENT_ID`) REFERENCES `USER` (`I
 
 -- ************************************** `IMAGE`
 
-CREATE TABLE `IMAGE`
+CREATE TABLE `image`
 (
  `ID`      integer NOT NULL AUTO_INCREMENT ,
  `PATH`    varchar(255) NOT NULL ,
  `POST_ID` integer NOT NULL ,
 
-PRIMARY KEY (`ID`),
-KEY `fkIdx_69` (`POST_ID`),
-CONSTRAINT `FK_69` FOREIGN KEY `fkIdx_69` (`POST_ID`) REFERENCES `POST` (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `fkIdx_69` (`POST_ID`),
+  CONSTRAINT `FK_69` FOREIGN KEY `fkIdx_69` (`POST_ID`) REFERENCES `post` (`ID`)
 );
 
 
 -- ************************************** `COMMENT`
 
-CREATE TABLE `COMMENT`
+CREATE TABLE `comment`
 (
  `ID`      integer NOT NULL AUTO_INCREMENT ,
  `COMMENT` varchar(255) NOT NULL ,
  `POST_ID` integer NOT NULL ,
  `USER_ID` integer NOT NULL ,
 
-PRIMARY KEY (`ID`),
-KEY `fkIdx_66` (`POST_ID`),
-CONSTRAINT `FK_66` FOREIGN KEY `fkIdx_66` (`POST_ID`) REFERENCES `POST` (`ID`),
-KEY `fkIdx_72` (`USER_ID`),
-CONSTRAINT `FK_72` FOREIGN KEY `fkIdx_72` (`USER_ID`) REFERENCES `USER` (`ID`)
+  PRIMARY KEY (`ID`),
+  KEY `fkIdx_66` (`POST_ID`),
+  CONSTRAINT `FK_66` FOREIGN KEY `fkIdx_66` (`POST_ID`) REFERENCES `post` (`ID`),
+  KEY `fkIdx_72` (`USER_ID`),
+  CONSTRAINT `FK_72` FOREIGN KEY `fkIdx_72` (`USER_ID`) REFERENCES `user` (`ID`)
 );
 
 
